15624e7578da2f7f07b1d1a63b70e9e585ad7449
mleiva2 <manuel.leiva@pmi.com>
2022-01-25 22:44:49 +0100

    Day 8 intro to functions, keywords arguments.

---
+++ Day8.py
@@ -0,0 +1,31 @@
+# # funtions
+# def greet():
+#     print("Hello")
+#     print("Hello2")
+#     print("Hello3")
+#
+#
+# greet()
+#
+#
+# # function that allows inputs
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print("Hello2")
+#     print("Hello3")
+#
+# greet_with_name("Manuel")
+
+# #Function with more than 1 input.
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"How is it in that {location}?")
+#
+# greet_with("Max", "US")
+
+#Keuwords arguments
+def greet_with(a, b):
+    print(f"Hello {a}")
+    print(f"How is it in that {b}?")
+
+greet_with(a="Max", b="US")
